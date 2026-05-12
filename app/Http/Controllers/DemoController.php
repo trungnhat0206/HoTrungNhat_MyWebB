da55c9ca1622fa0ab423c9a1cdf238a2f35c248c
trungnhat <trungnhat0206@gmail.com>
2026-05-12 15:54:51 +0700

    upload project

---
+++ app/Http/Controllers/DemoController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DemoController extends Controller
+{
+    public function index(){
+        return view("demo");
+    }
+    public function about(){
+        return view("about");
+    }
+
+    public function contact(){
+        return view("contact");
+    }
+}
